df2db2e3813cbe39cb4e42322d70d2daa92058cd
susu <drizzle_0505@outlook.com>
2024-06-10 16:49:40 +0800

    cursor movements

--- src/editor.rs
+++ src/editor.rs
@@ -1,17 +1,25 @@
 mod terminal;
 
-use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::event::{read, Event, Event::Key, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use terminal::Terminal;
 use std::io::Error;
 
+#[derive(Copy, Clone)]
+struct Location {
+    x: usize, 
+    y: usize, 
+}
+
 pub struct Editor {
     quitting: bool, 
+    location: Location, 
 }
 
 impl Editor {
     pub fn default() -> Self {
         Editor{
             quitting: false, 
+            location: Location{ x: 0, y: 0 }, 
         }
     }
 
@@ -41,14 +49,43 @@ impl Editor {
         Ok(())
     }
 
+    fn move_cursor_press(&mut self, code: KeyCode) -> Result<(), Error> {
+        let (cols, rows) = Terminal::size()?;
+        let cols = cols as usize;
+        let rows = rows as usize;
+        let Location { mut x, mut y } = self.location;
+
+        // window resizing?
+        match code {
+            KeyCode::Up => {
+                y = if y > 0 { y - 1 } else { 0 }
+            }, 
+            KeyCode::Down => {
+                y = if y < (rows - 1) { y + 1 } else { rows - 1 } 
+            }, 
+            KeyCode::Left => {
+                x = if x > 0 { x - 1 } else { 0 }
+            }, 
+            KeyCode::Right => {
+                x = if x < (cols - 1) { x + 1 } else { cols - 1 }
+            }, 
+            _ => (), 
+        }
+
+        self.location = Location { x, y };
+        Ok(())
+    }
+
     fn refresh_screen(&self) -> Result<(), Error> {
         Terminal::hide_cursor()?;
+        Terminal::move_cursor(0, 0)?;
         if self.quitting {
             Terminal::clear_screen()?;
             Terminal::print("End of editing! Byebye~\r\n")?;
         } else {
             Self::draw_tildes()?;
-            Terminal::move_cursor(0, 0)?;
+            // currently can be done like this.
+            Terminal::move_cursor(self.location.x.try_into().unwrap(), self.location.y.try_into().unwrap())?;
         }
         Terminal::show_cursor()?;
         Terminal::flush()?;
@@ -63,14 +100,22 @@ impl Editor {
         Terminal::terminate().unwrap();
     }
 
-    fn evaluate_event(&mut self, event: &Event) {
+    fn evaluate_event(&mut self, event: &Event) -> Result<(), Error> {
         if let Key(KeyEvent {
-            code: Char('q'), 
-            modifiers: KeyModifiers::CONTROL,
+            code, modifiers, kind: KeyEventKind::Press, 
             ..
         }) = event {
-            self.quitting = true;
+            match code {
+                KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.quitting = true;
+                },
+                KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right => {
+                    self.move_cursor_press(*code)?;
+                }, 
+                _ => (), 
+            }
         }
+        Ok(())
     }
 
     fn repl(&mut self) -> Result<(), Error> {
@@ -80,7 +125,7 @@ impl Editor {
                 break;
             }
             let event = read()?;
-            self.evaluate_event(&event);
+            self.evaluate_event(&event)?;
         }
         Ok(())
     }
